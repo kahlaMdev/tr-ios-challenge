59c0dd360023b922a2998feb1701dd9f3736b2f0
Kahla M <momoichat@gmail.com>
2021-10-02 19:07:31 -0400

    mkahla: Added MovieBrowser initial xCode project.

---
+++ MoviesBrowser/MoviesBrowser/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MoviesBrowser
+//
+//  Created by Mohamed Kahla on 2021-10-02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
